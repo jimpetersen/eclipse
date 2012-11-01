69f99484bd00814142408514fa06f4a56c8874f3
Jim Petersen <jim.petersen@live.se>
2012-11-01 13:25:52 +0100

    CP ROlL

---
+++ git git 2/src/git.java
@@ -0,0 +1,15 @@
+
+public class git {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("aswufhhwfhiwhfiueh");
+		System.out.println("FACEROLL RAGE!!!");
+
+	}
+
+}
