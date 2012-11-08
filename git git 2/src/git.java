546527c4f23a8b9eae1f6924aac4dacc20fc7c91
HedBye <Otto@Hedeby.it>
2012-11-08 11:18:25 +0100

    Update git git 2/src/git.java
    Kolla på rad 14

--- git git 2/src/git.java
+++ git git 2/src/git.java
@@ -11,6 +11,21 @@ public class git {
 		for(int i = 0; i<10; i++){
 			System.out.println("Nu loopar jag din loop "  + i);
 		}
+	// Här får du en array och for
+			int twoD[][]= new int[4][5];
+		    int i, j, k = 0;
+		    
+		    for(i=0; i<4; i++)
+		        for(j=0; j<5; j++) {
+		          twoD[i][j] = k;
+		          k++;
+		        }
+
+		      for(i=0; i<4; i++) {
+		        for(j=0; j<5; j++)
+		          System.out.print(twoD[i][j] + " ");
+		        System.out.println();
+		      }
 	}
 
 }
